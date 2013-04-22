1a9568acb3ea26896596953a324e8d4b8499eaa1
Mustack <jl.martin1990@gmail.com>
2013-04-22 00:25:40 -0400

    added playlist support (adding of playlists)

--- clitube.py
+++ clitube.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, and_
 from sqlalchemy.orm import sessionmaker
 
-from models import Video, Channel, create_tables
+from models import Video, Channel, Playlist, create_tables
 
 import ConfigParser
 
@@ -19,30 +19,65 @@ def add_channel(username, pref_name=None, regex=None):
 	import gdata.youtube.service
 	import re
 
+	def get_id(entry):
+		return entry.id.text[entry.id.text.rfind('/')+1:]
+
+	#The api does not return all results. This function does.
+	def get_full_feed(feed):
+		entries = feed.entry
+
+		while feed.GetNextLink():
+			feed = yt_service.GetYouTubePlaylistFeed(uri=feed.GetNextLink().href)
+			entries.extend(feed.entry)
+
+		return entries
+
+
 	yt_service = gdata.youtube.service.YouTubeService()
 	yt_service.developer_key = GOOGLE_DEVELOPER_KEY
 
 	uri = 'http://gdata.youtube.com/feeds/api/users/%s/uploads' % username
-	feed = yt_service.GetYouTubeVideoFeed(uri)
+	videos = get_full_feed(yt_service.GetYouTubeVideoFeed(uri=uri))
+
+	if not videos:
+		print 'ERROR: invalid youtube user (channel) name'
+
+	playlists = get_full_feed(yt_service.GetYouTubePlaylistFeed(username=username))
 
 	#Now for some sqlalchemy magic
 	chan = Channel(
 				name=username,
 				pref_name=pref_name
 			)
-	session.add(chan)
+	session.add(chan)		
 
 	session.add_all(
 		[Video(
-			name = entry.title.text,
-			url = entry.link[0].href,
-			channel = chan
-		) for entry in feed.entry if not regex or re.match(regex, entry.title.text)]
+			yt_id=get_id(video),
+			name=video.title.text,
+			channel=chan
+		) for video in videos if not regex or re.match(regex, video.title.text)]
 	)
 
-	session.commit()
+	for playlist in playlists:
+		pl = Playlist(
+				yt_id=get_id(playlist),
+				name=playlist.title.text,
+				channel=chan
+			)
+		session.add(pl)
 
-	return feed #in case you might want it
+		videos = get_full_feed(yt_service.GetYouTubePlaylistVideoFeed(playlist_id=pl.yt_id))
+
+		for index, video in enumerate(videos, start=1):
+			id = video.link[6].href[video.link[6].href.rfind('/')+1:]
+			vid = session.query(Video).filter(Video.yt_id==id)
+			vid.playlist_position = index
+			vid.playlist = pl
+			session.commit()
+
+
+	session.commit()
 
 def play_video(videos):
 	from subprocess import call
@@ -76,7 +111,7 @@ def play_channel(name, limit=1):
 		print "ERROR: "+ name +" is not a valid channel name"
 
 #useful for testing
-# create_tables(edngine)
-# add_channel('HuskyStarcraft', 'husky', r'(.*) vs (.*)')
+# create_tables(engine)
+# add_channel('GameGrumps', 'grumps')
 # play_video([session.query(Video).first()])
-# play_channel('husky', 17)
+play_channel('grumps', 17)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import ForeignKey, Column, Integer, String, Boolean
+from string import rfind
 
 Base = declarative_base()
 class Channel(Base):
@@ -8,30 +9,54 @@ class Channel(Base):
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String, nullable=False)
-	pref_name = Column(String)
+	pref_name = Column(String)  #TODO: enforce uniqueness across name and pref_name
 
 	videos = relationship("Video", backref=backref('channel', order_by=id))
 
 	def __init__(self, name, pref_name=None):
-		self.name = name
-		self.pref_name = pref_name
+		self.name = name.decode('utf-8')
+		self.pref_name = pref_name.decode('utf-8')
+
+#TODO: Inherit from Channel
+class Playlist(Base):
+	__tablename__ = 'playlists'
+
+	id = Column(Integer, primary_key=True)
+	yt_id = Column(String, nullable=False, unique=True)
+	name = Column(String, nullable=False, unique=True)
+	pref_name = Column(String)
+
+	channel_id = Column(Integer, ForeignKey('channels.id'))
+
+	videos = relationship("Video", backref=backref('playlist'))
+
+	def __init__(self, yt_id, name, channel):
+		self.yt_id = yt_id.decode('utf-8')
+		self.name = name.decode('utf-8')
+		self.channel = channel
 
 
 class Video(Base):
 	__tablename__ = 'videos'
 
 	id = Column(Integer, primary_key=True)
+	yt_id = Column(String, nullable=False, unique=True)
 	name = Column(String, nullable=False)
-	url = Column(String, nullable=False)
+	playlist_position = Column(Integer)
 	watched = Column(Boolean, nullable=False)
 
 	channel_id = Column(Integer, ForeignKey('channels.id'))
+	playlist_id = Column(String, ForeignKey('playlists.id'))
 
-	def __init__(self, name, url, channel):
-		self.name = name
-		self.url = url.replace('https:', 'http:')
+	def __init__(self, yt_id, name, channel):
+		self.yt_id = yt_id.decode('utf-8')
+		self.name = name.decode('utf-8')
 		self.watched = False
 		self.channel = channel
+
+	@property
+	def url(self):
+		return 'http://www.youtube.com/watch?v='+ self.yt_id
 
 def create_tables(engine):
 	Base.metadata.create_all(bind=engine)
